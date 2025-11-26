1b61e5105a292c529a73218810a5f342658bf2ae
unknown <gabrielalvaro673@gmail.com>
2025-11-26 12:01:04 +0800

    add data table

--- app/pages/inventory/index.vue
+++ app/pages/inventory/index.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    <h1>Inventory</h1>
-  </div>
+  <v-card
+    title="Inventory"
+    flat
+  >
+    <template v-slot:text>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      ></v-text-field>
+    </template>
+
+    <v-data-table
+      :headers="headers"
+      :items="inventory.data"
+      :search="search"
+    ></v-data-table>
+  </v-card>
 </template>
+<script setup>  
 
-<script lang="ts" setup>
 
+  const { data: inventory } = await useFetch('http://localhost:1337/api/inventories');
+
+  const search = ref('')
+  const headers = [
+    {key: 'product_name', title: 'product name'},
+    { key: 'product_description', title: 'Description' },
+    { key: 'quantity', title: 'quantity' },
+    { key : 'condition', title : 'condition'},
+    { key : 'location', title : 'location'},
+    { key : 'createdAt', title : 'Date created'},
+
+
+  ]
+  const desserts = [
+    
+  ]
 </script>
-
-<style>
-
-</style>
